4312e00b1c5c0a8eaf2408149f2c4544ae3430b1
Mahmuthan <mahmuthanb@gmail.com>
2025-08-27 12:38:00 +0300

    Set MainActivity to use HabituraAppRoot composable instead of HomeScreen directly

--- app/src/main/java/com/mahmuthan/habitura/MainActivity.kt
+++ app/src/main/java/com/mahmuthan/habitura/MainActivity.kt
@@ -3,10 +3,8 @@ package com.mahmuthan.habitura
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.mahmuthan.habitura.presentation.home.HomeScreen
-import com.mahmuthan.habitura.presentation.home.HomeViewModel
+import androidx.navigation.compose.rememberNavController
+import com.mahmuthan.habitura.presentation.navigation.NavGraph
 import com.mahmuthan.habitura.ui.theme.HabituraTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,10 +13,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HabituraTheme {
-                val homeViewModel: HomeViewModel = hiltViewModel()
-                HomeScreen(viewModel = homeViewModel)
-            }
+            HabituraAppRoot()
         }
     }
 }
