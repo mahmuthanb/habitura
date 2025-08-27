c16fe4dbf4be10c2a82c996c354bd583d4ef2ef3
Mahmuthan <mahmuthanb@gmail.com>
2025-08-27 12:37:48 +0300

    Add HabituraAppRoot composable and set up NavController and NavGraph

--- app/src/main/java/com/mahmuthan/habitura/HabituraApp.kt
+++ app/src/main/java/com/mahmuthan/habitura/HabituraApp.kt
@@ -1,7 +1,29 @@
 package com.mahmuthan.habitura
 
 import android.app.Application
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+import com.mahmuthan.habitura.presentation.navigation.NavGraph
+import com.mahmuthan.habitura.ui.theme.HabituraTheme
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class HabituraApp : Application()
+
+@Composable
+fun HabituraAppRoot() {
+    HabituraTheme {
+        val navController = rememberNavController()
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            // Just call NavGraph
+            NavGraph(navController = navController)
+        }
+    }
+}
