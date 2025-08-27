2dad3fa2bf903c9684f2400f1b25f75ead1bb41a
Mahmuthan <mahmuthanb@gmail.com>
2025-08-27 11:12:18 +0300

    Add UseCaseModule to provide HabitUseCases (insert, get, getById, delete)

---
+++ app/src/main/java/com/mahmuthan/habitura/di/UseCaseModule.kt
@@ -0,0 +1,25 @@
+package com.mahmuthan.habitura.di
+
+import com.mahmuthan.habitura.domain.repo.HabitRepository
+import com.mahmuthan.habitura.domain.usecase.*
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UseCaseModule {
+
+    @Provides
+    @Singleton
+    fun provideHabitUseCases(repository: HabitRepository): HabitUseCases {
+        return HabitUseCases(
+            insertHabit = InsertHabitUseCase(repository),
+            getHabits = GetHabitsUseCase(repository),
+            getHabitById = GetHabitByIdUseCase(repository),
+            deleteHabit = DeleteHabitUseCase(repository)
+        )
+    }
+}
